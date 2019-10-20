10e6bb87e8d354b830f1991bec1c9d396ac53a72
Jherson Medina <admedinaco@unal.edu.co>
2019-10-20 13:42:58 -0500

    Adding Bellman Ford Algorithm

---
+++ test/bellman_ford.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* * * *   * * *   	* * * */
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define ms(a,v) memset(a,v,sizeof a)
+/* * * *   * * *   	* * * */
+/* *
+ *
+ * Author: Hamkor a.k.a. OakenSh1eld
+ *
+ * */
+int main(){
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); 
+	int n; //Number of nodes;
+	int m; //Number of edges;
+	cin>>n>>m;
+	vector<pair<int, pii> edges; //Graph saved as an edge list
+	vecotr<int> dis(n, 1000000000) //Vector of distances, initially all are INF
+	int u; // Node u
+	int v; //Node v
+	int w; //weight
+	for(int i=0; i<m; ++i){
+		cin>>u>>v>>w;
+		edges.push_back({w, {u, v}});
+	}
+	//Bellman_ford Algorithm;
+	int x; cin>>x; //Source node;
+	dis[x] = 0;
+	for(int i=1; i<n; ++i){
+		for(auto e: edges){
+			w = e.first, a = e.second.first, b = e.second.second;
+			dis[b] = min(dis[b], dis[a] + 1);
+		}
+	}
+	return 0;
+}
