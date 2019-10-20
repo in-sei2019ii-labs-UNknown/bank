f81e75e80d74cd8332c26200a6ca2119d7b9433f
JhersonMedina <38172045+JhersonMedina@users.noreply.github.com>
2019-10-20 14:35:37 -0500

    Update bellman_ford.cpp

--- test/bellman_ford.cpp
+++ test/bellman_ford.cpp
@@ -16,14 +16,14 @@ int main(){
 	int n; //Number of nodes;
 	int m; //Number of edges;
 	cin>>n>>m;
-	vector<pair<int, pii> edges; //Graph saved as an edge list
-	vecotr<int> dis(n, 1000000000) //Vector of distances, initially all are INF
-	int u; // Node u
-	int v; //Node v
+	vector<pair<int, pii>> edges; //Graph saved as an edge list
+	vector<int> dis(n, 1000000000); //Vector of distances, initially all are INF
+	int a; // Node a
+	int b; //Node b
 	int w; //weight
 	for(int i=0; i<m; ++i){
-		cin>>u>>v>>w;
-		edges.push_back({w, {u, v}});
+		cin>>a>>b>>w;
+		edges.push_back({w, {a, b}});
 	}
 	//Bellman_ford Algorithm;
 	int x; cin>>x; //Source node;
@@ -31,8 +31,10 @@ int main(){
 	for(int i=1; i<n; ++i){
 		for(auto e: edges){
 			w = e.first, a = e.second.first, b = e.second.second;
-			dis[b] = min(dis[b], dis[a] + 1);
+			dis[b] = min(dis[b], dis[a] + w);
 		}
 	}
+	int y; cin>>y; //Objective node;
+	cout<<dis[y];
 	return 0;
 }
